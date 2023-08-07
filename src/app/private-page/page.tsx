d7c0c21f73041fc5519a77db285a24f4c580e107
LuisGilGB <luis.gilgb@gmail.com>
2023-08-07 20:39:16 +0200

    Configured middleware and private page to make it reachable only if the user is authenticated

---
+++ src/app/private-page/page.tsx
@@ -0,0 +1,9 @@
+const PrivatePage = () => (
+  <div className="w-full h-full flex justify-center items-center">
+    <p className="w-[720px] text-center">
+      This is a private page. You should be able to reach it if you&apos;re authenticated only.
+    </p>
+  </div>
+)
+
+export default PrivatePage
